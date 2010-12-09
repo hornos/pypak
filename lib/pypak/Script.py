6f5408d0cceda7c986795179fb71ca7649ba5794
Tom Hornos <tom.hornos@gmail.com>
2010-12-09 01:03:37 +0100

    init

---
+++ lib/pypak/Script.py
@@ -0,0 +1,113 @@
+#
+# Script
+#
+
+import string
+import ConfigParser
+from optparse import OptionParser
+import numpy
+from pypak.Types import *
+
+class Script( Debug ):
+  def __init__( self, scn = "pypak script", gpc = "pypak.ini" ):
+    Debug.__init__( self )
+    self.scn = scn
+    self.optpar = OptionParser()
+    self.gpc    = gpc
+    self.upc    = None
+    self.gpcpar = ConfigParser.ConfigParser()
+    self.upcpar = ConfigParser.ConfigParser()
+  # end def
+
+  def opt( self, *args, **kwargs ):
+    self.optpar.add_option( *args, **kwargs )
+  # end def
+
+  def par( self ):
+    return self.optpar.parse_args()
+  # end def
+
+  def ini( self ):
+    self.opt( "-c", "--config", action = "store", type = "string", dest = "upc", 
+              default = "config.ini", help = "User Configuration File (default: config.ini)" )
+
+    self.opt( "-v", "--verbose", action = "store_true", dest = "verbose", 
+              default = False, help = "Verbose Mode" )
+
+    self.opt( "-w", "--debug", action = "store_true", dest = "debug", 
+              default = False, help = "Debug Mode" )
+
+
+    (opts, args) = self.par()
+    self.verbose = opts.verbose
+    self.debug   = opts.debug
+    self.upc     = opts.upc
+
+    ## Global Config
+    try:
+      self.gpcpar.read( self.gpc )
+      if self.verbose :
+        print ' Global config: ' + self.gpc
+    except:
+      if self.verbose :
+        print ' Not Found: ' + self.gpc
+    # end try
+
+    ## Read User Config
+    try:
+      self.upcpar.read( self.upc )
+      if self.verbose :
+        print ' User config: ' + self.upc
+    except:
+      if self.verbose :
+        print ' Not Found: ' + self.upc
+    # end try
+  # end def
+
+
+  def cfg( self, item = None ):
+    try:
+      return self.upcpar.get( self.name, item )
+    except:
+      try:
+        return self.gpcpar.get( self.name, item )
+      except:
+        raise
+      # end try
+    # end try
+  # end def
+
+
+  def cfgi( self, item = None ):
+    return string.atoi( self.cfg( item ) )
+  # end def
+
+
+  def cfgf( self, item = None ):
+    return string.atof( self.cfg( item ) )
+  # end def
+
+
+  def cfgarr( self, item = None ):
+    line = self.cfg( item )
+    return line.split();
+  # end def
+
+
+  def cfgarri( self, item = None ):
+    arr = self.cfgarr( item )
+    for i in range( 0, len( arr ) ):
+      arr[i] = string.atoi( arr[i] )
+    # end for
+    return numpy.array( arr )
+  # end def
+
+
+  def cfgarrf( self, item = None ):
+    arr = self.cfgarr( item )
+    for i in range( 0, len( arr ) ):
+      arr[i] = string.atof( arr[i] )
+    # end for
+    return numpy.array( arr )
+  # end def
+# end class
---
+++ lib/pypak/Types.py
@@ -0,0 +1,163 @@
+#
+# Types package
+#
+
+import sys
+import copy
+import string
+import numpy
+
+class Enum( object ):
+  def __init__( self, elist ):
+    for num, name in enumerate( elist.split() ) :
+      setattr( self, name, num )
+    # end for
+  # end def
+# end class
+
+
+PosTypes = Enum( 'Direct Cart' )
+DictPosTypes = { PosTypes.Direct : 'Direct', PosTypes.Cart : 'Cart' }
+
+IOTypes = Enum( 'Input Output' )
+
+
+def TF( logical ):
+  if logical:
+    return 'T'
+  return 'F'
+# end def
+
+
+class Debug:
+  def __init__( self, verbose = False, debug = False ):
+    self.verbose = verbose
+    self.debug   = debug
+  # end def
+# end class
+
+
+class Buffer:
+  def __init__( self ):
+    self.lc    = -1
+    self.line  = ""
+    self.lines = {} 
+  # end def
+
+  def clean( self ):
+    self.lc = -1
+    self.lines = {}
+  # end def
+
+  def rewind( self ):
+    self.lc = -1
+  # end def
+
+  def read( self, lines = None ):
+    self.clean()
+    for line in lines:
+      self.lc += 1
+      self.lines[self.lc] = line.strip()
+    # end for
+    self.rewind()
+  # end def
+
+  def getline( self ):
+    self.lc += 1
+    self.line = self.lines[self.lc]
+    return self.line
+  # end def
+
+  def putline( self, line = None ):
+    self.lc += 1
+    self.line = line
+    self.lines[self.lc] = self.line
+  # end def
+# end class
+
+
+class Atom:
+  def __init__( self, symbol = "", no = 0, cl_shift = 0.000 ):
+    self.symbol = symbol
+    # not the atomic number !
+    self.no     = no
+    self.rno    = no
+    self.cl_shift = cl_shift
+  # end def
+# end class
+
+
+class VecPos:
+  def __init__( self, vec = numpy.zeros( 3 ), moveable = None ):
+    self.position  = numpy.array( vec )
+    self.force     = numpy.zeros( 3 )
+    self.velocity  = numpy.zeros( 3 )
+    self.moveable  = []
+    if( moveable == None ):
+      for i in range(0,3):
+        self.moveable.append( True )
+      # end for
+    else:
+      for m in moveable:
+        if string.upper( m ) == 'T':
+          self.moveable.append( True )
+        else:
+          self.moveable.append( False )
+        # end if
+      # end for
+    # end if
+  # end def
+# end class
+
+
+class AtomPos:
+  def __init__( self, atom = None, pos = None ):
+    self.atom = atom
+    self.pos  = pos
+  # end def
+
+  def symbol( self, newsym = None ):
+    if newsym != None:
+      self.atom.symbol = newsym
+    # end if
+    return self.atom.symbol
+  # end def
+
+  def position( self, newpos = None ):
+    if newpos != None:
+      self.pos.position = newpos
+    # end if
+    return self.pos.position
+  # end def
+
+  def moveable( self ):
+    return self.pos.moveable
+  # end def
+
+  def velocity( self ):
+    return self.pos.velocity
+  # end def
+
+  def no( self ):
+    return self.atom.no
+  # end def
+
+  def rno( self ):
+    return self.atom.rno
+  # end def
+
+  def cl_shift( self ):
+    return self.atom.cl_shift
+  # end def
+
+  def info( self ):
+    print " %4d" % self.no(), "%2s" % self.symbol(), self.position(), self.moveable()
+  # end def
+
+  def shift( self, S = numpy.zeros( 3 ) ):
+    for i in range( 0, 3 ):
+      self.pos.position[i] += S[i]
+    # end for
+  # end def
+# end class
+
