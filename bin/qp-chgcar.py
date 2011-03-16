b4625b33d33f0ef64108cc845e48c8bee907d388
Tom Hornos <tom.hornos@gmail.com>
2011-03-16 19:03:24 +0100

    fix

--- bin/qp-chgcar.py
+++ bin/qp-chgcar.py
@@ -5,6 +5,7 @@ import sys
 import string
 
 ### BEGIN HEADER
+script_name = os.path.basename( sys.argv[0] )
 sys.path.append( os.path.dirname( sys.argv[0] ) + "/../lib" )
 quantumpack_home   = os.path.dirname( sys.argv[0] ) + "/.."
 quantumpack_config = "qppak.ini"
